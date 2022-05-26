00e5d31d13c5358cbeac8c40a5650243c78c8663
Clebson Moura <clebson.sh@gmail.com>
2022-05-26 20:42:50 -0300

    rotas home e sign-in

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createServer } from "http";
+import { readFile } from "fs";
+import { resolve } from "path";
+import { parse } from "querystring";
 
 const server = createServer((request, response) => {
   switch (request.url) {
@@ -6,11 +9,54 @@ const server = createServer((request, response) => {
       response.writeHead(200, { "Content-Type": "application/json" });
       response.write(JSON.stringify({ status: "Okay" }));
       response.end();
-      return;
+      break;
+    case "/sign-in": {
+      const filePath = resolve(__dirname, "./pages/sign-in.html");
+      readFile(filePath, (err, file) => {
+        if (err) {
+          response.writeHead(500, "Can't pocess HTML file");
+          response.end();
+          return;
+        }
+
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      });
+      break;
+    }
+    case "/home": {
+      const filePath = resolve(__dirname, "./pages/home.html");
+      readFile(filePath, (err, file) => {
+        if (err) {
+          response.writeHead(500, "Can't pocess HTML file");
+          response.end();
+          return;
+        }
+
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      });
+      break;
+    }
+    case "/authenticate":
+      let data = "";
+      request.on("data", (chuck) => {
+        data += chuck;
+      });
+
+      request.on("end", () => {
+        const params = parse(data);
+        response.writeHead(301, {
+          Location: "/home",
+        });
+        response.end();
+      });
+      break;
     default:
       response.writeHead(404, "Service not found.");
       response.end();
-      return;
   }
 });
 
