317cb03150bff2e607d5135e717df92525a89aa2
Clebson Moura <clebson.sh@gmail.com>
2022-05-26 09:57:40 -0300

    adicionado nodemon para não precisar ficar reiniciando o server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import { createServer } from "http";
+
+const server = createServer();
+
+server.listen(8000, "127.0.0.1", () =>
+  console.log("Server is listening at http://127.0.0.1:8000")
+);
